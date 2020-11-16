b85e3f2c6a6670a0ff69644fe70da9ff0a409252
Ester Gama <esterfaniasantos@gmail.com>
2020-11-16 14:37:55 -0300

    Inclui ordenação dos pokemons por id

--- src/app/modules/pokemon/components/pokemon-card/pokemon-card.component.ts
+++ src/app/modules/pokemon/components/pokemon-card/pokemon-card.component.ts
@@ -1,10 +1,8 @@
 import { Pokemon } from './../../models/pokemon.model';
 import { Component, OnInit } from '@angular/core';
 import { PokemonService } from '../../services/pokemon.service';
-import { pluck } from 'rxjs/operators';
 import { TemplateRef } from '@angular/core';
 import { ViewChild } from '@angular/core';
-import { ModalService } from '../../../../shared/modal/modal.service';
 
 @Component({
   selector: 'app-pokemon-card',
@@ -13,7 +11,8 @@ import { ModalService } from '../../../../shared/modal/modal.service';
 })
 export class PokemonCardComponent implements OnInit {
   pokemons: Pokemon[] = [];
-  limite = 30;
+  limiteInitial = 30;
+  limiteDefault = 10;
   offset = 0;
   @ViewChild('modal') modalTemplateRef!: TemplateRef<any>;
   constructor(private pokemonService: PokemonService) {}
@@ -24,18 +23,22 @@ export class PokemonCardComponent implements OnInit {
   getImageUrl(id: number): string {
     return this.pokemonService.getPokeImageUrl(id);
   }
-  getPokemons(limit = this.limite, offset = this.offset): void {
+  getPokemons(limit = this.limiteInitial, offset = this.offset): void {
     this.pokemonService.getPokemons(limit, offset).subscribe((res) => {
       res.map((pokemon) =>
         pokemon.subscribe((i) => {
           this.pokemons.push(i);
+          this.pokemons.sort(this.sortById);
         })
       );
     });
   }
   getMorePokemons(): void {
-    this.offset = this.limite;
-    this.limite += 10;
-    this.getPokemons(this.limite, this.offset);
+    this.offset = this.limiteInitial;
+    this.limiteInitial += 10;
+    this.getPokemons(this.limiteDefault, this.offset);
+  }
+  sortById(a: Pokemon, b: Pokemon): number {
+    return a.id - b.id;
   }
 }
