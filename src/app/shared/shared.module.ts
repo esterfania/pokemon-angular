299d0d051dd4d4118f4ccd7d9b027d238f331a1b
Ester Gama <esterfaniasantos@gmail.com>
2020-11-18 23:04:21 -0300

    ajusta scss

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ModalComponent } from './modal/modal.component';
+import { ModalComponent } from './components/modal/modal.component';
 @NgModule({
   declarations: [ModalComponent],
   imports: [CommonModule],
